436403c01a48648854e224235018899f6283647f
Sen Mecatronique <109086855+Moustapha099@users.noreply.github.com>
2023-01-02 15:51:03 +0000

    Add files via upload

---
+++ BUZZER_PASSIF_CODE_tintinte/BUZZER_PASSIF_CODE_tintinte.ino
@@ -0,0 +1,15 @@
+int buzzer = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(buzzer, 1000, 200);
+  delay(100);
+    tone(buzzer, 0, 200);
+   delay(100);
+    tone(buzzer, 1000, 200);
+}
